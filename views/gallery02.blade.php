ec55ab95e0a34f3b946b5ab3f2102e16af11e475
Aadhaar Jain <56752657+Aadhaarjain@users.noreply.github.com>
2022-04-09 16:41:24 +0530

    Add files via upload

---
+++ views/gallery02.blade.php
@@ -0,0 +1,36 @@
+@include('layout.header')
+<!DOCTYPE html>
+<html>
+<title>ART MAGIC</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/gallery.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<section class="album">
+  <div class="inner-album">
+  <h3>ALBUMS</h3>
+  <h4>Mandala Art</h4>
+</div>
+</section>
+<!----gallery--->
+<div class="container">
+    <div class="box">
+      
+      <div class="pic">
+        <img src="images/gallery02/mandala1.jpg">
+      </div>
+      <div class="pic">
+            <img src="images/gallery02/mandala3.jpg">
+        </div>
+        <div class="pic">
+            <img src="images/gallery02/mandala2.jpg">
+        </div>
+    </div>
+</div>
+
+@include('layout.footer')
+
+</html>
---
+++ views/gallery05.blade.php
@@ -0,0 +1,38 @@
+@include('layout.header')
+<!DOCTYPE html>
+<html>
+<title>ART MAGIC</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/gallery.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<section class="album">
+  <div class="inner-album">
+  <h3>ALBUMS</h3>
+  <h4>Abstracts</h4>
+</div>
+</section>
+<!----gallery--->
+<div class="container">
+    <div class="box">
+      
+      <div class="pic">
+        <img src="images/gallery05/abstract1.jpg">
+      </div>
+      <div class="pic">
+             <img src="images/gallery05/abstract3.jpg">
+        </div>
+
+        <div class="pic">
+             <img src="images/gallery05/abstract2.jpg">
+        </div>
+    </div>
+</div>
+
+@include('layout.footer')
+
+</html>
